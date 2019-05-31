4ed3e3490de73314eacfd63dc70fdbeca3a2339a
Joao Vitor Ximenez <jvximenez@gmail.com>
2019-05-31 19:11:04 -0300

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCS77TuCe2jrXqmSDh9Ivs7gWedPM7PJ3E",
+    authDomain: "ionic3-gastos-45fd4.firebaseapp.com",
+    databaseURL: "https://ionic3-gastos-45fd4.firebaseio.com",
+    projectId: "ionic3-gastos-45fd4",
+    storageBucket: "ionic3-gastos-45fd4.appspot.com",
+    messagingSenderId: "404912444334"
+  }
+
 };
