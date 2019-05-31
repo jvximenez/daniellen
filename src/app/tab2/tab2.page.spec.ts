391ce021c393909e8006cd4ebb8a8f3794aaa058
Joao Vitor Ximenez <jvximenez@gmail.com>
2019-05-31 19:03:07 -0300

    first

--- src/app/tab2/tab2.page.spec.ts
+++ src/app/tab2/tab2.page.spec.ts
@@ -3,7 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Tab2Page } from './tab2.page';
 
-describe('Tab2Page', () => {
+describe('Gastos', () => {
   let component: Tab2Page;
   let fixture: ComponentFixture<Tab2Page>;
 
