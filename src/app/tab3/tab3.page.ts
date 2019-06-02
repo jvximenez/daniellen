3cddc6b40bcadfd658a434d984be0f41144be4d1
Joao Vitor Ximenez <jvximenez@gmail.com>
2019-06-02 13:25:36 -0300

    criando pagina de restaures

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,39 +1,23 @@
 import { Component } from '@angular/core';
-import  * as firebase  from 'firebase/app';
+import * as firebase from 'firebase';
+
 
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
+
+  
 })
+
 export class Tab3Page {
-  countryRef: any
+  comprasRef: any
   countryList: any
-  country
 
-  constructor() {4
-    this.countryRef = firebase.database().ref('/compras').limitToLast(100).orderByChild("total")
 
-  
-
-    this.countryRef.on('value', countryList => {
-        
-      let countries = [];
-      countryList.forEach( country => { 
-        var obj
-        obj = country.val()
-        obj.key = country.key
-        countries.push(obj);
-        
-        return false;
-      });
-      countries = countries.reverse()
-  
-      this.countryList = countries;
-      console.log(this.countryList)
-    });
-  
+  constructor() {
+      
   }
-
 }
+
