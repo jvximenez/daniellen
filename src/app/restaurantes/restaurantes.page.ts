0796c072120d29a52d25fd578fbf8c240d5cbd6a
Joao Vitor Ximenez <jvximenez@gmail.com>
2019-06-02 21:55:31 -0300

    fim

--- src/app/restaurantes/restaurantes.page.ts
+++ src/app/restaurantes/restaurantes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-restaurantes',
@@ -6,8 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./restaurantes.page.scss'],
 })
 export class RestaurantesPage implements OnInit {
+  restaurantesRef:any
+  restaurantesList :any
+  restaurante: any
 
-  constructor() { }
+  constructor() { 
+    this.restaurantesRef = firebase.database().ref('/restaures').orderByChild('total')
+    
+    this.restaurantesRef.on('value', restaurantesList =>{
+      let restaurantes = [];
+      restaurantesList.forEach(restaurante => {
+        var obj
+        obj = restaurante.val()
+        console.log(obj['cia']),
+        (obj['cia'].forEach(element => {if (element == 'Daniela'){
+          obj.key = restaurante.key
+          restaurantes.push(obj)
+        }
+      }))
+
+        return false;
+        
+      });
+      restaurantes = restaurantes.reverse()
+
+      this.restaurantesList = restaurantes
+      console.log(this.restaurantesList)
+    })
+  
+  }
 
   ngOnInit() {
   }
